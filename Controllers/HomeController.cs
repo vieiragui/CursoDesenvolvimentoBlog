77ea0de606986b5f2813f63fd28333606dd33261
Guilherme Vieira <vieiragui21@gmail.com>
2022-07-25 21:27:17 -0300

    Aula 15 Exibindo todos os posts na index

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,26 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using CursoDesenvolvimentoWeb.Models;
+using CursoDesenvolvimentoWeb.Repository.Interfaces;
+using System.Threading.Tasks;
 
 namespace CursoDesenvolvimentoWeb.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IRepositoryBase<Post> _postRepository;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IRepositoryBase<Post> postRepository)
         {
             _logger = logger;
+            _postRepository = postRepository;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var allPosts = await _postRepository.All();
+            return View(allPosts);
         }
 
         public IActionResult Privacy()
